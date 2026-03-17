dd01ca622a30beb4d0d73266d351f5b5588c2d72
raidelo <raidelosix@gmail.com>
2026-03-16 21:58:00 -0400

    feat: basic implementation of the UDP echo server

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -3,6 +3,8 @@ from socket import AddressFamily, SocketKind, socket
 from threading import Thread
 from typing import Generator
 
+CHUNK = 64
+
 
 def process_server(args: Namespace) -> int:
 
@@ -43,13 +45,12 @@ def handle_stream(connection: socket, address: tuple[str, int]) -> None:
     for line in readlines(connection):
         print(f" > {line}")
 
-        respond(connection, line)
+        respond_stream(connection, line)
 
     connection.close()
 
 
 def readlines(connection: socket) -> Generator[bytes, None, None]:
-    CHUNK = 64
     line = b""
     while True:
         received = connection.recv(CHUNK)
@@ -63,11 +64,30 @@ def readlines(connection: socket) -> Generator[bytes, None, None]:
             yield y
 
 
-def respond(connection: socket, content: bytes) -> None:
+def respond_stream(connection: socket, content: bytes) -> None:
     connection.sendall(content)
 
 
 def socket_dgram(address: tuple[str, int]) -> None:
     sock = socket(AddressFamily.AF_INET, SocketKind.SOCK_DGRAM)
 
-    raise NotImplementedError()
+    sock.bind(address)
+
+    print(f"[*] UDP Server listening at: {address}")
+
+    while True:
+        data, address = sock.recvfrom(CHUNK)
+        handle_dgram(sock, data, address)
+
+
+def handle_dgram(sock: socket, data: bytes, address: tuple[str, int]) -> None:
+    print(f"[*] Data received from: {address}")
+    print("    Content:")
+
+    print(f" > {data}")
+
+    respond_dgram(sock, address, data)
+
+
+def respond_dgram(sock: socket, address: tuple[str, int], content: bytes) -> None:
+    sock.sendto(content, address)
