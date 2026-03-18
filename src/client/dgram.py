2fced8a7c8300728d16ac82a5b0da3d92fdab53a
raidelo <raidelosix@gmail.com>
2026-03-18 00:43:22 -0400

    feat: basic implementation of the UDP echo client

--- src/client/dgram.py
+++ src/client/dgram.py
@@ -1,2 +1,39 @@
+from os import isatty
+from socket import AddressFamily, SocketKind, socket
+from sys import stdin
+
+from ..constants import CHUNK
+
+
 def client_dgram(address: tuple[str, int]) -> None:
-    raise NotImplementedError()
+    sock = socket(AddressFamily.AF_INET, SocketKind.SOCK_DGRAM)
+    sock.settimeout(0.001)
+
+    if isatty(stdin.fileno()):
+        while True:
+            data = stdin.readline()
+            sock.sendto(data.encode(), address)
+
+            try:
+                data, address = sock.recvfrom(CHUNK)
+                print(data)
+            except TimeoutError:
+                continue
+
+    else:
+        data = stdin.read()
+        sock.sendto(data.encode(), address)
+
+        print(f"[*] UDP packet sent to: {address}")
+
+        print(f"[*] Sent {address}:")
+        print(f" > {data.replace('\n', '\\n')}")
+
+        try:
+            data, address = sock.recvfrom(CHUNK)
+            print(f"[*] Received {address}:")
+            print(f" > {data}")
+        except TimeoutError:
+            print("[*] No response received")
+
+    sock.close()
